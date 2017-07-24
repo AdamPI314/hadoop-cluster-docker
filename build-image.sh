b2716458819bd27670182817047abd8e5799dbee
DRdotEVIL18 <AdamPI314@users.noreply.github.com>
2017-07-24 17:25:37 -0600

    Remove intermediate files after building.

--- build-image.sh
+++ build-image.sh
@@ -3,6 +3,6 @@
 echo ""
 
 echo -e "\nbuild docker hadoop image\n"
-sudo docker build -t drdotevil18/scratch:hadoop-cluster-1.0.0 .
+sudo docker build -t drdotevil18/scratch:hadoop-cluster-1.0.0 --rm .
 
 echo ""
